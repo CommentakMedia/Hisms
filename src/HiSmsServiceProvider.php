8e86ea1571ded546564ee42eb5ce305ba8dc553d
Amro Khaled | Commentatk <hi@amrography.com>
2021-06-03 13:52:32 +0200

    hot fix

--- src/HiSmsServiceProvider.php
+++ src/HiSmsServiceProvider.php
@@ -30,7 +30,7 @@ class HiSmsServiceProvider extends ServiceProvider
     {
         $this->app->singleton('hisms', function ($app) {
             $config = $app['config']['jawalbsms'] ?: $app['config']['jawalbsms::config'];
-            \Log::info($config);
+            \Log::info("HISMS", $config);
             $client = new HismsClient($config['Username'], $config['Password'], $config['SenderName']);
             return $client;
         });
